a1c550a1022ca8bebe8711f11067dc549fd58a27
Felipe Baz <54612024+Felipe-Baz@users.noreply.github.com>
2024-08-12 19:51:49 -0300

    Update supplychain.sol

--- supplychain.sol
+++ supplychain.sol
@@ -1,76 +1,154 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
 
 contract SupplyChain { 
-    address public owner;
-
-    enum SupplyChainState { 
-        Created, 
-        Paid, 
-        Delivered,
-        Received
+    enum ShipmentStatus { 
+        PENDING, 
+        IN_TRANSIT,
+        DELIVERED
     }
 
-    struct Order { 
-        string productName; 
-        uint256 amount; 
-        uint256 price; 
-        address buyer; 
-        address seller; 
-        SupplyChainState state; 
+    struct Shipment { 
+        address sender; 
+        address receiver; 
+        uint256 pickupTime; 
+        uint256 deliveryTime; 
+        uint256 distance;
+        uint256 price;
+        ShipmentStatus status;
+        bool isPaid;
     } 
 
-    mapping (uint256 => Order) public orders;
+    mapping(address => Shipment[]) public shipments;
+    uint256 public shipmentCount;
 
-    uint256 public orderCounter; 
-
-    event OrderCreated(uint256 orderID); 
-    event OrderPaid(uint256 orderID); 
-    event OrderDelivered(uint256 orderID); 
-    event OrderConfirmReceive(uint256 orderID);
-    event QuantityUpdated(uint256 orderID, uint256 newAmount);
-
-    constructor() { 
-        owner = msg.sender; 
+    struct TypeShipment {
+        address sender; 
+        address receiver;
+        uint256 pickupTime; 
+        uint256 deliveryTime; 
+        uint256 distance;
+        uint256 price;
+        ShipmentStatus status;
+        bool isPaid;
     }
 
-    function createOrder(string memory _productName, uint256 _amount, uint256 _price) public { 
-        require(msg.sender != address(0), "Invalid address"); 
-        orderCounter++; 
-        orders[orderCounter] = Order(_productName, _amount, _price, msg.sender, address(0), SupplyChainState.Created); 
-        emit OrderCreated(orderCounter); 
-    } 
+    TypeShipment[] typeShipments;
 
-    function payForOrder(uint256 _orderID) public payable { 
-        require(msg.sender != address(0), "Invalid address"); 
-        require(msg.value == orders[_orderID].price, "Invalid payment amount"); 
-        require(orders[_orderID].state == SupplyChainState.Created, "Invalid order state"); 
-        orders[_orderID].seller = msg.sender; 
-        orders[_orderID].state = SupplyChainState.Paid; 
-        payable(owner).transfer(msg.value); 
-        emit OrderPaid(_orderID); 
+    event ShipmentCreated (
+        address indexed sender, 
+        address indexed receiver, 
+        uint256 pickupTime,
+        uint256 distance,
+        uint256 price
+    );
+
+    event ShipmentInTransit (
+        address indexed sender, 
+        address indexed receiver, 
+        uint256 pickupTime
+    );
+
+    event ShipmentDelivered (
+        address indexed sender, 
+        address indexed receiver, 
+        uint256 deliveryTime
+    );
+
+    event ShipmentPaid (
+        address indexed sender, 
+        address indexed receiver, 
+        uint256 amount
+    );
+
+    constructor() {
+        shipmentCount = 0;
     }
 
-    function deliverOrder(uint256 _orderID) public { 
-        require(msg.sender != address(0), "Invalid address"); 
-        require(msg.sender == orders[_orderID].buyer || msg.sender == orders[_orderID].seller, "Unauthorized user"); 
-        require(orders[_orderID].state == SupplyChainState.Paid, "Invalid order state"); 
-        orders[_orderID].state = SupplyChainState.Delivered; 
-        emit OrderDelivered(_orderID); 
+    function createShipment(address _receiver, uint256 _pickupTime, uint256 _distance, uint256 _price) public payable {
+        require(msg.value == _price, "O pagamento deve ser igual ao valor do pedido.");
+
+        Shipment memory shipment = Shipment(
+            msg.sender, 
+            _receiver, 
+            _pickupTime, 
+            0, 
+            _distance, 
+            _price, 
+            ShipmentStatus.PENDING, 
+            false
+        );
+
+        shipments[msg.sender].push(shipment);
+        shipmentCount++;
+
+        typeShipments.push(
+            TypeShipment(
+                msg.sender,
+                _receiver,
+                _pickupTime, 
+                0, 
+                _distance, 
+                _price, 
+                ShipmentStatus.PENDING, 
+                false
+            )
+        );
+
+        emit ShipmentCreated(msg.sender, _receiver, _pickupTime, _distance, _price);
     }
 
-    function confirmReceiveOrder(uint256 _orderID) public { 
-        require(msg.sender != address(0), "Invalid address"); 
-        require(msg.sender == orders[_orderID].buyer || msg.sender == orders[_orderID].seller, "Unauthorized user"); 
-        require(orders[_orderID].state == SupplyChainState.Delivered, "Invalid order state"); 
-        orders[_orderID].state = SupplyChainState.Received; 
-        emit OrderConfirmReceive(_orderID); 
+    function startShipment(address _sender, address _receiver, uint256 _index) public  {
+        Shipment storage shipment = shipments[_sender][_index];
+        TypeShipment storage  typeShipment = typeShipments[_index];
+        require(shipment.receiver == _receiver, "Receiver invalido.");
+        require(shipment.status == ShipmentStatus.PENDING, "O pedido ja esta em transito.");
+
+        shipment.status = ShipmentStatus.IN_TRANSIT;
+        typeShipment.status = ShipmentStatus.IN_TRANSIT;
+
+        emit ShipmentDelivered(_sender, _receiver, shipment.pickupTime);
     }
 
-    function updateQuantity(uint256 _orderID, uint256 _newAmount) public {
-        require(orders[_orderID].state == SupplyChainState.Created, "Invalid order state");
-        orders[_orderID].amount = _newAmount;
-        emit QuantityUpdated(_orderID, _newAmount);
+    function completeShipment(address _sender, address _receiver, uint256 _index) public {
+        Shipment storage shipment = shipments[_sender][_index];
+        TypeShipment storage typeShipment = typeShipments[_index];
+
+        require(shipment.receiver == _receiver, "Invalid Receiver.");
+        require(shipment.status == ShipmentStatus.IN_TRANSIT, "O Pedido nao esta em transito.");
+        require(!shipment.isPaid, "O Pedido ja foi pago.");
+
+        shipment.status = ShipmentStatus.DELIVERED;
+        typeShipment.status = ShipmentStatus.DELIVERED;
+
+        typeShipment.deliveryTime = block.timestamp;
+
+        uint256 amount = shipment.price;
+
+        payable(shipment.sender).transfer(amount);
+
+        shipment.isPaid = true;
+        typeShipment.isPaid = true;
+
+        emit ShipmentDelivered(_sender, _receiver, shipment.deliveryTime);
+        emit ShipmentPaid(_sender, _receiver, amount);
+    }
+
+    function getShipment(address _sender, uint256 _index) public view returns (address, address, uint256, uint256, uint256, uint256, ShipmentStatus, bool) {
+        Shipment memory shipment = shipments[_sender][_index];
+        return (shipment.sender, shipment.receiver, shipment.pickupTime, shipment.deliveryTime, shipment.distance, shipment.price, shipment.status, shipment.isPaid);
+    }
+
+    function getShipmentCount(address _sender) public view returns (uint256) {
+        return shipments[_sender].length;
+    }
+
+    function getAllTransactions ()
+        public 
+        view 
+        returns (TypeShipment[] memory)
+    {
+        return typeShipments;
     }
 }
